7cdc8a2d39545e411f53d9b7356c9baf5f16c6c5
LilBliP <140349870+LilBliP@users.noreply.github.com>
2024-03-07 11:22:11 +1300

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,6 +1,7 @@
 /*
  * use p5.js to draw a clock on a 960x500 canvas
  */
+
 function draw_clock(obj) {
   // draw your own clock here based on the values of obj:
   //    obj.hours goes from 0-23
@@ -11,6 +12,16 @@ function draw_clock(obj) {
   //        < 0 if no alarm is set
   //        = 0 if the alarm is currently going off
   //        > 0 --> the number of seconds until alarm should go off
+  let hours = obj.hours;
+let minutes = obj.minutes;
+let seconds = obj.seconds;
+let millis = obj.millis;
+let alarm = obj.seconds_until_alarm;
+
+let secondgearrot = seconds + (millis / 1000.0);
+let minutegearrot = minutes + ((seconds + millis/1000)/60)
+
+
   translate(width/2, height/2)
   background(50); //  beige
   fill(200); // dark grey
@@ -23,7 +34,7 @@ angleMode(DEGREES)
   ellipse(-125, 0, 100);
 
 push()
-rotate(360 / 60 * obj.minutes)
+rotate(360 / secondgearrot)
   fill(140, 255, 251) // blue
   ellipse(0, 0, 150);
 
@@ -31,6 +42,8 @@ rotate(360 / 60 * obj.minutes)
    rotate(360/9)
     ellipse(0,75,20);
   }
+  fill(255)
+  ellipse(0,75,20);
 pop()
 
   fill(175, 133, 255); // purple
